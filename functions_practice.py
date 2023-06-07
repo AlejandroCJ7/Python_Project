c8108eb4540f058f78b37859ed0e1b72acd7181b
Alex Castaneda <alex@Alejandros-Mac.local>
2023-06-06 23:19:06 -0700

    fixed my indentations

--- functions_practice.py
+++ functions_practice.py
@@ -1,11 +1,22 @@
 def hello():
     print('hello user!!')
 
+def pack(a, b, c):
+    return [a, b, c]
 
+def eat_lunch(favorite_food):
+    if len(favorite_food) == 0:
+        print('My lunch box is empty!')
+    else:
+        for i in range(len(favorite_food)):
+            if i == 0:
+                print(f'First, I eat {favorite_food[0]}')
+            else:
+                print(f'Next, I eat {favorite_food[i]}')
 
-    def pack(spaghetti, tuna, pizza):
-        return [spaghetti, tuna, pizza]
-
-
-
-        def eat_lunch(favorite_food)
+hello()
+print(pack("a", "b", "c"))
+print(pack(1, 2, 3))
+eat_lunch([])
+eat_lunch(["spaghetti"])
+eat_lunch(["spaghetti", "bread", "tuna", "pizza"])
